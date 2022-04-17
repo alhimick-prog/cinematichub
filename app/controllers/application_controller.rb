5d456835c378106676dc3d67b0be8f62ecc91ab5
alhimick-prog <alhimick96@yahoo.com>
2022-04-17 22:57:56 +0300

    Gem devise; registration, login, logout pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:email,
+                       :encrypted_password,
+                       :name,
+                       :nickname,
+                       :birthday]
+    )
+    devise_parameter_sanitizer.permit(
+      :account_update, keys: [:password,
+                              :password_confirmation,
+                              :name,
+                              :nickname,
+                              :birthday,
+                              :current_password]
+    )
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum role: { admin: 0, editor: 1, subscriber: 2 }
+end
